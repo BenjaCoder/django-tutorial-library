cbf8e061634d0a12199ee5414185f01d72a9c815
BenjaCoder <ben.hamula@gmail.com>
2025-08-29 16:59:05 -0400

    Modifies models

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -17,6 +17,7 @@ class Genre(models.Model):
 
     class Meta:
         constraints = [UniqueConstraint(Lower('name'), name='genre_name_case_insensitive_unique', violation_error_message="Genre already exists (case insensitive match)")]
+        ordering = ['name']
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -24,12 +25,19 @@ class Book(models.Model):
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
     isbn = models.CharField('ISBN', max_length=13, unique=True, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    language = models.ForeignKey('language', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    class Meta:
+        ordering = ['title']
 
 class BookInstance(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
@@ -47,7 +55,36 @@ class BookInstance(models.Model):
     status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text="Book availability")
 
     class Meta:
-        ordering = ['due_back']
+        ordering = ['-due_back']
+        # ordering = ['book', 'imprint']
 
     def __str__(self):
-        return f'{self.id} ({self.book.title})'
+        return f'{self.book.title} ({self.imprint})'
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+    
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+
+class Language(models.Model):
+    name = models.CharField(max_length=200, unique=True, help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def get_absolute_url(self):
+        return reverse('language-detail', args=[str(self.id)])
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        constraints = [UniqueConstraint(Lower('name'), name='language_name_case_insenstive_unique', violation_error_message="Language already exists (case insensitve match)")]
+        ordering = ['name']
